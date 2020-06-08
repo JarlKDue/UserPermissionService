6bdfb199fb67aa7b44cbce6fcc2258ec2cfbbc1e
jkd <jkd@miracle.dk>
2020-06-08 20:18:36 +0200

    Updating Rest Endpoint

--- src/main/java/com/miracle/userpermissionservice/ActiveDirectorySearchInterface.java
+++ src/main/java/com/miracle/userpermissionservice/ActiveDirectorySearchInterface.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
@@ -13,30 +14,52 @@ public interface ActiveDirectorySearchInterface {
 
     static boolean shouldUserBeAdmin(DirContext ctx, String accountName) {
         String searchFilter = "(objectClass=inetOrgPerson)";
-        final String ldapSearchBase = "OU=3SCALE,OU=Funktioner,OU=Standard,DC=eniig,DC=org";
-
-        SearchControls searchControls = new SearchControls();
-        searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        String[] reqAtt = { "cn", "sn" };
+        SearchControls controls = new SearchControls();
+        controls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        controls.setReturningAttributes(reqAtt);
 
         try {
-            NamingEnumeration<SearchResult> results = ctx.search(ldapSearchBase, searchFilter, searchControls);
-            SearchResult searchResult = null;
-            if(results.hasMoreElements()) {
-                searchResult = results.nextElement();
-                System.out.println(searchResult.toString());
-
-                if(results.hasMoreElements()) {
-                    System.err.println("Matched multiple users for the accountName: " + accountName);
-                    return false;
-                }
+            NamingEnumeration users = ctx.search("ou=users,ou=system", searchFilter, controls);
+            SearchResult result = null;
+            while (users.hasMore()) {
+                result = (SearchResult) users.next();
+                Attributes attr = result.getAttributes();
+                String name = attr.get("cn").get(0).toString();
+                //deleteUserFromGroup(name,"Administrators");
+                System.out.println(attr.get("cn"));
+                System.out.println(attr.get("sn"));
             }
-            return true;
-
-        } catch (NamingException e){
-            System.out.println(e.getMessage());
+        } catch (NamingException e) {
+            e.printStackTrace();
         }
 
-        System.out.println("Returning True!!");
+
+//        String searchFilter = "(objectClass=inetOrgPerson)";
+//        final String ldapSearchBase = "OU=3SCALE,OU=Funktioner,OU=Standard,DC=eniig,DC=org";
+//
+//        SearchControls searchControls = new SearchControls();
+//        searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+//
+//        try {
+//            NamingEnumeration<SearchResult> results = ctx.search(ldapSearchBase, searchFilter, searchControls);
+//            SearchResult searchResult = null;
+//            if(results.hasMoreElements()) {
+//                searchResult = results.nextElement();
+//                System.out.println(searchResult.toString());
+//
+//                if(results.hasMoreElements()) {
+//                    System.err.println("Matched multiple users for the accountName: " + accountName);
+//                    return false;
+//                }
+//            }
+//            return true;
+//
+//        } catch (NamingException e){
+//            System.out.println(e.getMessage());
+//        }
+//
+//        System.out.println("Returning True!!");
         return true;
     }
 }
