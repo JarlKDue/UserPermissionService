8060ed30a973935404b993a363bb3f5d81cbc8d1
jkd <jkd@miracle.dk>
2020-06-08 21:40:07 +0200

    Updating Models

--- src/main/java/com/miracle/userpermissionservice/RestServlet.java
+++ src/main/java/com/miracle/userpermissionservice/RestServlet.java
@@ -3,6 +3,12 @@ package com.miracle.userpermissionservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.NodeList;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 @RestController
 @Component
@@ -12,8 +18,10 @@ public class RestServlet {
     GetLDAPConnectionBean getLDAPConnectionBean;
 
     @PostMapping(value = "/update_user_permissions", consumes = "application/xml")
-    public void updateUserPermissions(@RequestBody String threeScaleMessage){
-        System.out.println("Received Request to Validate " + threeScaleMessage);
+    public void updateUserPermissions(@RequestBody String threeScaleMessage) throws XPathExpressionException {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        NodeList nodeList = (NodeList) xPath.compile("//email[1]/text()").evaluate(threeScaleMessage, XPathConstants.NODESET);
+        System.out.println("Received Request to Validate " + nodeList.item(0).toString());
         getLDAPConnectionBean.getDirContext().ifPresent(
                 context -> ActiveDirectorySearchInterface.shouldUserBeAdmin(context, threeScaleMessage)
         );
