8365043c5deec365194467ba542011937e3f6d92
jkd <jkd@miracle.dk>
2020-06-08 15:11:22 +0200

    Starting Project

---
+++ src/main/java/com/miracle/userpermissionservice/ActiveDirectorySearchInterface.java
@@ -0,0 +1,33 @@
+package com.miracle.userpermissionservice;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.LdapContext;
+
+public interface ActiveDirectorySearchInterface {
+
+    static boolean shouldUserBeAdmin(DirContext ctx, String ldapSearchBase, String accountName) throws NamingException {
+        String searchFilter = "(&(objectClass=user)(sAMAccountName=" + accountName + "))";
+
+        SearchControls searchControls = new SearchControls();
+        searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+
+        NamingEnumeration<SearchResult> results = ctx.search(ldapSearchBase, searchFilter, searchControls);
+
+        SearchResult searchResult = null;
+        if(results.hasMoreElements()) {
+            searchResult = (SearchResult) results.nextElement();
+
+            //make sure there is not another item available, there should be only 1 match
+            if(results.hasMoreElements()) {
+                System.err.println("Matched multiple users for the accountName: " + accountName);
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/com/miracle/userpermissionservice/UserPermissionService.java
@@ -0,0 +1,37 @@
+package com.miracle.userpermissionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Hashtable;
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
+
+@SpringBootApplication
+public class UserPermissionService {
+
+    public static void main(String[] args) throws NamingException {
+        SpringApplication.run(UserPermissionService.class, args);
+
+
+    final String ldapAdServer = "ldap://eniig.org:389";
+    final String ldapSearchBase = "OU=3SCALE,OU=Funktioner,OU=Standard,DC=eniig,DC=org";
+
+    final String ldapUsername = "OpenShift";
+    final String ldapPassword = "Such$Y9t2RiBicuQ!p*j";
+
+    Hashtable<String, Object> env = new Hashtable<String, Object>();
+        env.put(Context.SECURITY_AUTHENTICATION, "simple");
+        env.put(Context.SECURITY_PRINCIPAL, ldapUsername);
+        env.put(Context.SECURITY_CREDENTIALS, ldapPassword);
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.PROVIDER_URL, ldapAdServer);
+
+        env.put("java.naming.ldap.attributes.binary", "objectSID");
+
+
+    LdapContext ctx = new InitialLdapContext();
+
+    }
+}
