f1b8e205f8b0ce4419d2daac79c11449bd5b1d37
jkd <jkd@miracle.dk>
2020-06-08 16:45:40 +0200

    Adding Actuator Endpoints

--- src/main/java/com/miracle/userpermissionservice/RestServlet.java
+++ src/main/java/com/miracle/userpermissionservice/RestServlet.java
@@ -1,6 +1,7 @@
 package com.miracle.userpermissionservice;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,10 @@ public class RestServlet {
     @PostMapping(value = "/update_user_permissions", consumes = "applicat/xml")
     public void updateUserPermissions(@RequestParam String incoming){
         System.out.println(incoming);
+    }
+
+    @GetMapping(value = "check_health")
+    public void checkHealth(){
+        System.out.println("Alive and Kicking");
     }
 }
