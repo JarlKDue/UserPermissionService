0a14c1595d2160f4f75c46e44406b391afbb3590
jkd <jkd@miracle.dk>
2020-06-09 08:02:34 +0200

    Changing input to XML

--- src/main/java/com/miracle/userpermissionservice/RestServlet.java
+++ src/main/java/com/miracle/userpermissionservice/RestServlet.java
@@ -1,15 +1,22 @@
 package com.miracle.userpermissionservice;
 
-import jdk.internal.org.xml.sax.InputSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
+import java.io.IOException;
 import java.io.StringReader;
 
 @RestController
@@ -20,12 +27,17 @@ public class RestServlet {
     GetLDAPConnectionBean getLDAPConnectionBean;
 
     @PostMapping(value = "/update_user_permissions", consumes = "application/xml")
-    public void updateUserPermissions(@RequestBody String threeScaleMessage) throws XPathExpressionException {
-        XPath xPath = XPathFactory.newInstance().newXPath();
-        InputSource inputXML = new InputSource( new StringReader( threeScaleMessage ) );
+    public void updateUserPermissions(@RequestBody String threeScaleMessage) throws XPathExpressionException, ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+        Document document = docBuilder.parse(new InputSource(new StringReader(threeScaleMessage)));
+        System.out.println(document.getXmlVersion());
 
-        String email = (String) xPath.compile("//email[1]/text()").evaluate(inputXML, XPathConstants.STRING);
-        System.out.println("Received Request to Validate " + email);
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        String expression = "//email[1]/text()";
+        Node node = (Node) xPath.compile(expression).evaluate(document, XPathConstants.NODE);
+        System.out.println(node.getNodeValue());
+        System.out.println("Received Request to Validate " + node.getNodeValue());
         getLDAPConnectionBean.getDirContext().ifPresent(
                 context -> ActiveDirectorySearchInterface.shouldUserBeAdmin(context, threeScaleMessage)
         );
