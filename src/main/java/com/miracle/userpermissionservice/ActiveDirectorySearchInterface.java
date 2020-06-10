a04afd5377c1eea73256d857d65a2fa1afe1d9d3
jkd <jkd@miracle.dk>
2020-06-10 19:22:06 +0200

    Updating AD Searches

--- src/main/java/com/miracle/userpermissionservice/ActiveDirectorySearchInterface.java
+++ src/main/java/com/miracle/userpermissionservice/ActiveDirectorySearchInterface.java
@@ -18,8 +18,8 @@ public interface ActiveDirectorySearchInterface {
 
     static List<String> getInternalEmails(DirContext ctx, String group) {
         List<String> memberEmails = new ArrayList<>();
-        String searchFilter = "(memberOf:1.2.840.113556.1.4.1941:=CN=" + group + ",OU=3SCALE,OU=Funktioner,OU=Standard,DC=eniig,DC=org)";
-        String[] reqAtt = { "mail"};
+        String searchFilter = "(&(objectCategory=user)(memberOf:1.2.840.113556.1.4.1941:=CN=" + group + ",OU=3SCALE,OU=Funktioner,OU=Standard,DC=eniig,DC=org))";
+        String[] reqAtt = { "userPrincipalName"};
         SearchControls controls = new SearchControls();
         controls.setSearchScope(SearchControls.SUBTREE_SCOPE);
         controls.setReturningAttributes(reqAtt);
@@ -41,8 +41,8 @@ public interface ActiveDirectorySearchInterface {
 
     static List<String> getExternalEmails(DirContext ctx, String group) {
         List<String> memberEmails = new ArrayList<>();
-        String searchFilter = "(memberOf:1.2.840.113556.1.4.1941:=CN=" + group + ",OU=3SCALE,OU=Funktioner,OU=Eksterne,DC=eniig,DC=org)";
-        String[] reqAtt = { "mail"};
+        String searchFilter = "(&(objectCategory=user)(memberOf:1.2.840.113556.1.4.1941:=CN=" + group + ",OU=3SCALE,OU=Funktioner,OU=Standard,DC=eniig,DC=org))";
+        String[] reqAtt = { "userPrincipalName"};
         SearchControls controls = new SearchControls();
         controls.setSearchScope(SearchControls.SUBTREE_SCOPE);
         controls.setReturningAttributes(reqAtt);
