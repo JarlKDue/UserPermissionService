7d1f94f30cee30d174a6c886c3c4b8bc2de3e236
jkd <jkd@miracle.dk>
2020-06-10 12:28:01 +0200

    Updating Manager Flow

--- src/main/java/com/miracle/userpermissionservice/ThreeScaleApiInterface.java
+++ src/main/java/com/miracle/userpermissionservice/ThreeScaleApiInterface.java
@@ -117,7 +117,7 @@ public interface ThreeScaleApiInterface {
             URI uri = new URIBuilder(request.getURI())
                     .addParameter("access_token", threeScaleAccessToken)
                     .addParameter("allowed_service_ids[]", "")
-                    .addParameter("allowed_sections[]", "{portal, finance, settings, partners, monitoring, plans, policy_registry}")
+                    .addParameter("allowed_sections[]", "portal, finance, settings, partners, monitoring, plans, policy_registry")
                     .build();
             request.setURI(uri);
             HttpResponse response = httpClient.execute(request);
