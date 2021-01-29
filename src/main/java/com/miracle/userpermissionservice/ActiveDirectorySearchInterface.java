980508233927d3392d21d8591e78fc485d1e6140
jkd <jkd@miracle.dk>
2021-01-29 11:03:31 +0100

    Removing search criteria

--- src/main/java/com/miracle/userpermissionservice/ActiveDirectorySearchInterface.java
+++ src/main/java/com/miracle/userpermissionservice/ActiveDirectorySearchInterface.java
@@ -70,7 +70,7 @@ public interface ActiveDirectorySearchInterface {
 
     static NamingEnumeration<SearchResult> completeSearchForInternalUsers(DirContext ctx, String searchFilter, SearchControls controls){
         try{
-            return ctx.search("OU=Eniig_Faellesfunktioner,DC=eniig,DC=org", searchFilter, controls);
+            return ctx.search("DC=eniig,DC=org", searchFilter, controls);
         } catch (NamingException e) {
             e.printStackTrace();
         }
